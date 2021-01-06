6bdb078af6d86a30b1420c652b9b863d5c893ab9
Aron HD <60329603+Aron-HD@users.noreply.github.com>
2021-01-06 17:34:00 +0000

    tested with inputs. Refined get winners class to take other options for different csvs. Comments throughout and some reordering of data dict values

--- sample/core.py
+++ sample/core.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd # comment this once get_csv moved?
 from glob import glob
 from pathlib import Path
 
@@ -10,59 +10,73 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(
 	loader=FileSystemLoader('../templates'))
 
-### GUI Inputs
+### CLI / GUI Inputs
 values = {}
 values['date'] = '29/10/2020'
-values['page'] = "special"
-values['cat'] = "growth" # make this able to pass in a list of cats too
-values['award'] = "effectiveness"
+values['page'] = "winners"
+values['cat'] = "data" # make this able to pass in a list of cats too
+values['award'] = "media"
 values['entry_kit'] = "MENAPrize2020-entrykit_v2.pdf"
 values['report_link'] = "/content/article/2020-mena-strategy-report-insights-from-the-warc-prize-for-mena-strategy/133904"
 
-def get_winners(cat):
-	try:
-		print('reading winners csv')
-		winners = pd.read_csv(f'../data/csv/{cat}_winners.csv').to_dict('records')
-		return winners
-	except UnicodeError as e:
-		winners = pd.read_csv(f'../data/csv/{cat}_winners.csv', encoding='cp1252').to_dict('records')
-		return winners
-	except FileNotFoundError:
-		print('no winners papers')
-		return False
-		# raise e
+# init as class
+data = {}
 
+def get_csv(cat, page): # swap page for path 
+	'''Reads in csv data for shortlist, winners or judges.'''
+	print('reading csv')
+	try: 
+		path = { # pass in path object in respective functions when class
+			'shortlist': f'../data/csv/shortlists/{cat}_shortlist.csv',
+			'winners': f'../data/csv/{cat}_winners.csv',
+			'judges': f'../data/csv/{cat}-judges.csv'
+		}
+		df = pd.read_csv(path[page])
+		return df.to_dict('records')
+	except UnicodeError:
+		df = pd.read_csv(path[page], encoding='cp1252')
+		return df.to_dict('records')
+	except KeyError:
+		print('no csv read')
+		return False
+
+# make this a class TemplateData: that imports from package /template_data with helpers in too
 def get_data():
 			
-	data = {}
-
+	# init as class common ones have rest as functions
 	date = values['date']
 	awd = values['award']
 	cat = values['cat']
 	page = values['page']
 
-	# init as class
-	data.update({	                              # get each elmt using awd_elmt() from AWARDS dictionary
-		"code":        AWARDS[awd]['code'],	      # and take inputs from gui window and feed into temporary 
-		"cartridge":   AWARDS[awd]['cartridge'],  # data dictionary for rendering template
-		"award":       awd,
-		"entry_kit":   values['entry_kit'].replace('.pdf',''),
-		"report_link": values['report_link'],
-		"report":      AWARDS[awd]['categories'][cat]['report'],
-		"full_award":  AWARDS[awd]['full_award'],
+	data.update({	                               # get each elmt using awd_elmt() from AWARDS dictionary
+		"award":       awd,                        # and take input from gui window / cli and feed into temporary 
+		"cat":         cat,                        # data dictionary for rendering template
 		"year":        func.process_date(date)[2], # should shrink this to just get date from now
-		page:          func.awd_elmt(awd, cat, page),
-		"papers":      get_winners(cat),
-		"cat":         cat,
+		 page:         func.awd_elmt(awd, cat, page),
+		"code":        AWARDS[awd]['code'],	      
+		"cartridge":   AWARDS[awd]['cartridge'],
+		"entry_kit":   values['entry_kit'].replace('.pdf',''),
+		"report":      AWARDS[awd]['categories'][cat]['report'],
+		"report_link": values['report_link'],
+		"full_award":  AWARDS[awd]['full_award'],
 	  	"special_awards": AWARDS[awd]['categories'][cat]['special_awards'],
-	  	# "special_awards": ?,
 	})
 
+	# {papers / judges / winners / shortlist}
+	# have each as individual function on GUI / 
+	# if radio input is true then get_csv that thing
+	# get_csv(cat, page) if value for value in values True else pass
+	# so that only necessary info is added to dict
+	data.update({"papers": get_csv(cat, page)})
+	# def function(): 
+	'''To sort judges bios.'''
 	# chair? for testimonial
-	# chair = func.get_chair(category=cat)
+	# chair = get_chair(category=cat)
 
 	# pics and bios should be covered in above
 	# also need to read in csvs
+	# data.update({"judges": ?})
 
 	return data, page
 
